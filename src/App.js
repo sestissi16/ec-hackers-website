9a9e596a50e1e13196245adefd8470387c390b8e
Sari Stissi <s3937stissi@gmail.com>
2017-12-15 15:02:15 -0500

    Made it so the App component filled the whole screen and added some background colors

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div className="Header">
           <h1 className="App-title">Welcome to React</h1>
-        </header>
+        </div>
+        <div className="Body">
+          <h2>This is the body</h2>
+        </div>
       </div>
     );
   }
