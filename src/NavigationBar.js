580e5a9edb50271c9d0339b736ee60afe873d452
Sari Stissi <s3937stissi@gmail.com>
2017-12-19 22:51:30 -0800

    Fixed the styling on the links in the nav bar

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -6,9 +6,9 @@ class NavigationBar extends Component {
     render() {
         return(
             <div className="menu">
-                <div className= "link1"><Link to="/Home" >Home</Link></div>
-                <div className="link2"><Link to="/SignUp">SignUp</Link></div>
-                <div className="link3"><Link to="/Info">Info</Link></div>
+                <div className= "link1"><Link to="/Home" className="Link1" >Home</Link></div>
+                <div className="link2"><Link to="/SignUp" className="Link2">SignUp</Link></div>
+                <div className="link3"><Link to="/Info" className="Link3">Info</Link></div>
             </div>
         );
     }
