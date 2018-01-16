b0b7d24b4964913914a015684c4fabed6bcac9bf
Sari S <sestissi16@earlham.edu>
2018-01-16 11:49:38 -0500

    Tried grid system on HomePage and installed react-media so I could use media queries and tested that out

--- src/HomePage.js
+++ src/HomePage.js
@@ -9,10 +9,10 @@ class HomePage extends Component {
             <div className="Body">
                 {/* <Header /> */}
                 <NavigationBar />
-                <div className="Content">
-                    <h1>Welcome Hackers!</h1>
-                    <img src="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F37204292%2F160311289377%2F1%2Foriginal.jpg?w=1000&rect=52%2C0%2C1610%2C805&s=334ecf41e9082d9798bb143e603aa123" alt="hackathon pic" className="HackathonPic"/>
-                    <img src="http://www.athens.edu/images/coas/programs/computer-science.jpg?x75869" alt="computer science pic" className="RandoPic"/>
+                <div className="Content row">
+                    <h1 claasName="column small-2 medium-4">Welcome Hackers!</h1>
+                    <img src="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F37204292%2F160311289377%2F1%2Foriginal.jpg?w=1000&rect=52%2C0%2C1610%2C805&s=334ecf41e9082d9798bb143e603aa123" alt="hackathon pic" className="HackathonPic column small-2 medium 4"/>
+                    <img src="http://www.athens.edu/images/coas/programs/computer-science.jpg?x75869" alt="computer science pic" className="RandoPic column small-2 medium-4"/>
                 </div>
             </div>
         );
--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -2,20 +2,32 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './NavigationBar.css';
 import SquirrelHackIcon from './finished-squirrel-hack-logo-transparent.png'
+import Media from 'react-media';
 
 class NavigationBar extends Component {
     render() {
         return(
-            <div className="menu">
-                <img src={SquirrelHackIcon} alt="hackathon logo" className="NavLogoImg" />
-                <p className="AppTitle">Squirrel Hack</p>
-                <div className= "link1"><Link to="/" className="Link1" >Home</Link></div>
-                <div className="link2"><Link to="/SignUp" className="Link2">SignUp</Link></div>
-                <div className="link3"><Link to="/About" className="Link3">About</Link></div>
-                <div className="link4"><Link to="/FAQ" className="Link4"> FAQ </Link></div>
-                <div className="link5"><Link to="/Schedule" className="Link5"> Schedule </Link></div>
-                <div className="link6"><Link to="/Sponsors" className="Link6"> Sponsors </Link></div>
-                <img src='http://www.performanceservices.com/files/image/earlham-college-logo.png' alt="Earlham College logo" className="NavECLogo" />
+            <div>
+                <Media query={{maxWidth:1024}}>
+                    {
+                        matches => matches ? (
+                            <p>Smaller Screen</p>
+                        ) : (
+                            <p>Bigger Screen</p>
+                        )
+                    }
+                </Media>
+                <div className="menu">
+                    <img src={SquirrelHackIcon} alt="hackathon logo" className="NavLogoImg" />
+                    <p className="AppTitle">Squirrel Hack</p>
+                    <div className= "link1"><Link to="/" className="Link1" >Home</Link></div>
+                    <div className="link2"><Link to="/SignUp" className="Link2">SignUp</Link></div>
+                    <div className="link3"><Link to="/About" className="Link3">About</Link></div>
+                    <div className="link4"><Link to="/FAQ" className="Link4"> FAQ </Link></div>
+                    <div className="link5"><Link to="/Schedule" className="Link5"> Schedule </Link></div>
+                    <div className="link6"><Link to="/Sponsors" className="Link6"> Sponsors </Link></div>
+                    <img src='http://www.performanceservices.com/files/image/earlham-college-logo.png' alt="Earlham College logo" className="NavECLogo" />
+                </div>
             </div>
         );
     }
