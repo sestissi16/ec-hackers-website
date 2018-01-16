fc32881022835135aedbc828fd1b067075380248
Sari S <sestissi16@earlham.edu>
2018-01-16 15:46:12 -0500

    Start of nav bar being responsive with media query

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -13,21 +13,21 @@ class NavigationBar extends Component {
                         matches => matches ? (
                             <p>Smaller Screen</p>
                         ) : (
-                            <p>Bigger Screen</p>
+                            <div className="menu">
+                                <img src={SquirrelHackIcon} alt="hackathon logo" className="NavLogoImg" />
+                                <p className="AppTitle">Squirrel Hack</p>
+                                <div className= "link1"><Link to="/" className="Link1" >Home</Link></div>
+                                <div className="link2"><Link to="/SignUp" className="Link2">SignUp</Link></div>
+                                <div className="link3"><Link to="/About" className="Link3">About</Link></div>
+                                <div className="link4"><Link to="/FAQ" className="Link4"> FAQ </Link></div>
+                                <div className="link5"><Link to="/Schedule" className="Link5"> Schedule </Link></div>
+                                <div className="link6"><Link to="/Sponsors" className="Link6"> Sponsors </Link></div>
+                                <img src='http://www.performanceservices.com/files/image/earlham-college-logo.png' alt="Earlham College logo" className="NavECLogo" />
+                            </div>
                         )
                     }
                 </Media>
-                <div className="menu">
-                    <img src={SquirrelHackIcon} alt="hackathon logo" className="NavLogoImg" />
-                    <p className="AppTitle">Squirrel Hack</p>
-                    <div className= "link1"><Link to="/" className="Link1" >Home</Link></div>
-                    <div className="link2"><Link to="/SignUp" className="Link2">SignUp</Link></div>
-                    <div className="link3"><Link to="/About" className="Link3">About</Link></div>
-                    <div className="link4"><Link to="/FAQ" className="Link4"> FAQ </Link></div>
-                    <div className="link5"><Link to="/Schedule" className="Link5"> Schedule </Link></div>
-                    <div className="link6"><Link to="/Sponsors" className="Link6"> Sponsors </Link></div>
-                    <img src='http://www.performanceservices.com/files/image/earlham-college-logo.png' alt="Earlham College logo" className="NavECLogo" />
-                </div>
+                
             </div>
         );
     }
