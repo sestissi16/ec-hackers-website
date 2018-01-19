0b011ff03b015370954ddf680839d0743d8989dd
Sari S <sestissi16@earlham.edu>
2018-01-18 21:37:08 -0500

    Tried to embed form but just linked to it instead

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react';
 import './SignUp.css';
 import NavigationBar from './NavigationBar.js';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
 class SignUp extends Component {
     render() {
+        const html = '<div class="typeform-widget" data-url="https://saris7.typeform.com/to/G1GYux" style="width: 100%; height: 500px;"></div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script> <div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;"> powered by <a href="https://admin.typeform.com/signup?utm_campaign=G1GYux&utm_source=typeform.com-11518443-Basic&utm_medium=typeform&utm_content=typeform-embedded-poweredbytypeform&utm_term=EN" style="color: #999" target="_blank">Typeform</a> </div>'
         return(
             <div className="SignUpBody">
                 <NavigationBar />
                 <div className="SignUpContent">
-                    <h1>Welcome to SignUp Page!</h1>
-                    <p> This is where you'll sign up for the Hackathon.</p>
-                    <div className="NoteForm">
-                        <p>
-                            <input
-                                
-                            />
-                        </p>
-                    </div>
+                    {/* <div> { ReactHtmlParser(html) } </div> */}
+                    <h1>So you want to join in on the fun?</h1>
+                    <h2>Follow the link below to sign up and be apart of the Squirrel Hack!</h2>
+                    <a href="https://saris7.typeform.com/to/G1GYux">Sign Up Here!</a>
                 </div>
             </div>
         );
