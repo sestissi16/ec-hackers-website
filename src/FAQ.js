feac85b8a565a19f3208a15e6de954927d068f16
Sari S <sestissi16@earlham.edu>
2018-01-24 15:36:50 -0500

    Added word content to FAQ page

--- src/FAQ.js
+++ src/FAQ.js
@@ -9,7 +9,8 @@ class FAQ extends Component {
             <div className="FAQPage">
                 <NavigationBar />
                 <div className="FAQContent">
-                    <p>Welcome to the FAQ</p>
+                    <h1 className="FAQTitle">Welcome to the FAQ</h1>
+                    <h2 className="FAQSub">Send us questions at <span><u>ecsquirrelhacks@gmail.com</u></span> and check back to see our answers!</h2>
                 </div>
             </div>
         );
