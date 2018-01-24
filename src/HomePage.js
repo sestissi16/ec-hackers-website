c47d4453b993fd77141ef850336ecd2c62c3d7ae
Sari S <sestissi16@earlham.edu>
2018-01-24 15:27:07 -0500

    Added Logo and Word content to HomePage

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './HomePage.css';
 import NavigationBar from './NavigationBar.js';
-import Header from './Header.js';
+import SquirrelLogo from './finished-squirrel-hack-logo-transparent.png'
 
 class HomePage extends Component {
     render() {
         return(
             <div className="Body">
-                {/* <Header /> */}
                 <NavigationBar />
                 <div className="Content row">
-                    <h1 claasName="column small-2 medium-4">Welcome Hackers!</h1>
-                    <img src="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F37204292%2F160311289377%2F1%2Foriginal.jpg?w=1000&rect=52%2C0%2C1610%2C805&s=334ecf41e9082d9798bb143e603aa123" alt="hackathon pic" className="HackathonPic column small-2 medium 4"/>
-                    <img src="http://www.athens.edu/images/coas/programs/computer-science.jpg?x75869" alt="computer science pic" className="RandoPic column small-2 medium-4"/>
+                    <img className="LogoPic column small-1 medium-3" src={SquirrelLogo} alt="Squirrel Hack Logo" />
+                    <div className="WordContent">
+                        <h1 claasName="column small-2 medium-4" id="WelcomeTitle">Welcome to Squirrel Hacks!</h1>
+                        <h2 className="WelcomeSub">Earlham College's inaugural Hackathon</h2>
+                        <h3 className="TagLine">We're excited to offer a socially responsible <em>24 hours</em> of exciting healthy hacking!</h3>
+                    </div>
                 </div>
             </div>
         );
