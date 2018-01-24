27027b76e8c8b7dbf41cb6bb0b085c96524caefa
Sari S <sestissi16@earlham.edu>
2018-01-24 17:19:54 -0500

    Added content to sponsor page

--- src/Sponsors.js
+++ src/Sponsors.js
@@ -8,7 +8,24 @@ class Sponsors extends Component {
             <div className="SponsorsPage">
                 <NavigationBar />
                 <div className="SponsorsContent">
-                    <p>Welcome to the Sponsors Page</p>
+                    <h1  className="SponsorsTitle">Welcome to the Sponsors Page</h1>
+                    <h2 className="SponsorsSub">How can you make it happen or get involved?</h2>
+                    <h3 className="SponsorsListTitle">You can support the event in the following forms:</h3>
+                    <ul className="BigList">
+                        <li>Sponsorships for</li>
+                        <ul className="SmallList">
+                            <li>Meals,</li>
+                            <li>Prizes,</li>
+                            <li>Logistics,</li>
+                            <li>Tech for use during the hackathon.</li>
+                        </ul>
+                        <li>Attending and helping with, the event by:</li>
+                        <ul className="SmallList">
+                            <li>Judging the competition,</li>
+                            <li>Mentorship</li>
+                            <li>Voluntary services</li>
+                        </ul>
+                    </ul>
                 </div>
             </div>
         );
